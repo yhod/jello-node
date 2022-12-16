b0b3a337ec0b5a9557b7911e43e96b4886c4c45e
Blue Turtle <myblueturtle76@gmail.com>
2022-12-16 10:35:07 -0800

    Update config.js

--- database/config/config.js
+++ database/config/config.js
@@ -5,7 +5,7 @@ module.exports = {
         'database': 'simple_app_db',
         'host': '127.0.0.1',
         'dialect': 'postgres',
-        logging: true,
+        logging: false,
     },
     'test': {
         'username': 'simpleuser',
